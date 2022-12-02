34a67ac974fdcf9408458300189bf121d32e51e4
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-02 09:55:21 +0600

    router setup has been done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routes/Routes/routes';
 
 function App() {
   return (
     <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Routes/Routes/routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layouts/Main/Main";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>
+    }
+])
+export default router;
