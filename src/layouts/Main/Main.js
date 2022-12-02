611ed6c183389be70bf1dd6ebda10d78a350cf1c
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-02 10:05:39 +0600

    added signup background image in Main layout

--- src/layouts/Main/Main.js
+++ src/layouts/Main/Main.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import signUpBgImage from '../../assets/SignUpBGImage/SignUpBGImage.png';
 
 const Main = () => {
     return (
         <div>
-
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row gap-20">
+                    <img src={signUpBgImage} alt="" className="rounded-lg" />
+                    <div>
+                        <h1>Signup Form</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
