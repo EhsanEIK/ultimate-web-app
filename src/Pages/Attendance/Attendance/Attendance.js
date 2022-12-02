ccfa71212e8108084e1db5104f78ba852fc78221
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-02 21:15:03 +0600

    loaded all the info in table in Attendance page

--- src/Pages/Attendance/Attendance/Attendance.js
+++ src/Pages/Attendance/Attendance/Attendance.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../../assets/logo/ultimate hrm logo.png';
 
 const Attendance = () => {
+    const [attendanceInfo, setAttendanceInfo] = useState([]);
+    useEffect(() => {
+        fetch('https://test.nexisltd.com/test', {
+            headers: {
+                authorization: `bearer ${localStorage.getItem('ultimateAccessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                setAttendanceInfo(data)
+            })
+    }, [])
+
+    let attendanceInfoArray = [];
+
+    for (const key in attendanceInfo) {
+        let attendanceArrayStatusList = [];
+        let attendanceArrayDateList = [];
+
+        const value = attendanceInfo[key];
+        const attendanceList = value.attendance;
+
+        // get the attendance date list of a person and set them into attendanceArrayDateList
+        attendanceArrayDateList = Object.keys(value.attendance);
+
+        // get the attendance status list of a person and set them into attendanceArrayStatusList
+        for (const key2 in attendanceList) {
+            attendanceArrayStatusList.push(attendanceList[key2]);
+        }
+
+        // set a new property in attendanceInfoArray for attendance date list and attendance status
+        value.attendanceArrayList = attendanceArrayStatusList;
+        value.attendanceArrayDateList = attendanceArrayDateList
+        attendanceInfoArray.push(value)
+    }
+
     return (
         <div className='container mx-auto'>
             <div className="navbar bg-base-100">
@@ -17,16 +53,22 @@ const Attendance = () => {
                             <th></th>
                             <th>Date</th>
                             <th>Employeer Name</th>
+                            <th>Position</th>
                             <th>Status</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="hover">
-                            <th>1</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
+                        {
+                            attendanceInfoArray.map((info, idx) =>
+                                <tr key={info.id} className="hover">
+                                    <th>{idx + 1}</th>
+                                    <td>{info?.attendanceArrayDateList[25]}</td>
+                                    <td>{info?.name}</td>
+                                    <td>{info?.position}</td>
+                                    <td className='capitalize'>{info?.attendanceArrayList[25]?.status}</td>
+                                </tr>
+                            )
+                        }
                     </tbody>
                 </table>
             </div>
